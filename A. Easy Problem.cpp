045627a724c4feaa54caad41484dfd0beabda4cf
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-04-05 10:19:13 +0600

    Create A. Easy Problem.cpp

---
+++ A. Easy Problem.cpp
@@ -0,0 +1,10 @@
+//ata language Kotlin 1.7.20
+//c++ convert korleo hobe
+
+fun main() {
+    val t = readLine()!!.toInt()
+    repeat(t) {
+        val n = readLine()!!.toInt()
+        println(n - 1)
+    }
+}
