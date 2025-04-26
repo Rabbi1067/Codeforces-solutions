0b793c47812ea749b690003bdc51ee8608831fac
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-04-26 10:47:31 +0600

    Create 2057A - MEX Table.cpp

---
+++ 2057A - MEX Table.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int x,y;
+        cin>>x>>y;
+        cout<<max(x,y)+1<<endl;
+    }
+return 0;
+}
