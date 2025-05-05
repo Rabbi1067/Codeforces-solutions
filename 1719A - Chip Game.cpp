565f9a7c42ea70f96f7960818adfdc1145fec8c4
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-05-05 23:02:00 +0600

    Create 1719A - Chip Game.cpp

---
+++ 1719A - Chip Game.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    long long n,m;
+    cin>>n>>m;
+    if((n+m)%2) cout<<"Burenka"<<endl;
+    else cout<<"Tonya"<<endl;
+
+}
+}
